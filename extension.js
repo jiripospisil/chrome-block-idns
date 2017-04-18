a6beccf56d9b32cd41642f814a7d34c8c1e7746b
Jiri Pospisil <mekishizufu@gmail.com>
2017-04-18 23:44:02 +0200

    Fix indentation

--- extension.js
+++ extension.js
@@ -1,12 +1,11 @@
 chrome.webRequest.onBeforeRequest.addListener(details => {
-    try {
-        const url = new URL(details.url);
-        const hasIdn = url.hostname.split('.').some(label => label.startsWith('xn--'));
+  try {
+    const url = new URL(details.url);
+    const hasIdn = url.hostname.split('.').some(label => label.startsWith('xn--'));
 
-        return { cancel: hasIdn };
-    } catch(err) {
-        console.error(err.stack);
-        return { cancel: false };
-    }
-
+    return { cancel: hasIdn };
+  } catch (err) {
+    console.error(err.stack);
+    return { cancel: false };
+  }
 }, { urls: ['<all_urls>'] }, ['blocking']);
